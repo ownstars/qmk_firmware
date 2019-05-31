ff1f3fb8fa90b0faac976e0331ab82ff81a559ab
yimao <yimao@yimao.me>
2019-05-31 18:32:51 +0800

    Explain the use of keymap rules

--- keyboards/hhkb/keymaps/yimao/rules.mk
+++ keyboards/hhkb/keymaps/yimao/rules.mk
@@ -1,2 +1,3 @@
-LINK_TIME_OPTIMIZATION_ENABLE = yes
-# HHKB_RN42_ENABLE=yes
+LINK_TIME_OPTIMIZATION_ENABLE = yes # Minimize firmware size
+# HHKB_RN42_ENABLE=yes # HHKB_RN42_ENABLE rule not working in keymap folder, set it in keyboard folder or use HHKB_RN42_ENABLE=yes while compiling.
+# CONSOLE_ENABLE = no # Console can not be disabled while using bluetooth.
