b9854014ce392eb8a12eb878899612495318bf9b
yimao <yimao@yimao.me>
2019-06-13 10:49:54 +0800

    Add mac layer, use leader key 1, 2 to sway

--- keyboards/hhkb/keymaps/yimao/keymap.c
+++ keyboards/hhkb/keymaps/yimao/keymap.c
@@ -4,8 +4,9 @@
 #include QMK_KEYBOARD_H
 
 #define BASE 0
-#define HHKB 1
-#define MOUSE 2
+#define MAC 1
+#define HHKB 2
+#define MOUSE 3
 
 enum hhkb_keycodes
 {
@@ -24,17 +25,24 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Contro|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Enter   |
      * |-----------------------------------------------------------|
-     * |Shift(  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift)|Gui|
+     * |Shift(  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift)|LEA|
      * `-----------------------------------------------------------'
-     *       |MO1|Alt  |         Space         |MO1  |LEA|
+     *       |MO1|Alt  |         Space         |MO1  |Gui|
      *       `-------------------------------------------'
     */
     [BASE] = LAYOUT( //  default layer
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, 
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, 
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(MOUSE, KC_SCLN), KC_QUOT, KC_ENT, 
-        KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_RGUI, 
-        MO(HHKB), KC_LALT, KC_SPC, MO(HHKB), KC_LEAD),
+        KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSPC, KC_LEAD, 
+        MO(HHKB), KC_LALT, KC_SPC, MO(HHKB), KC_RGUI),
+
+    [MAC] = LAYOUT( //  default layer
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LT(MOUSE, KC_SCLN), KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+        KC_TRNS, KC_LGUI, KC_TRNS, KC_TRNS, KC_LALT),
 
 
     /* HHKB Level (HHKB Fn)
@@ -131,7 +139,11 @@ void matrix_scan_user(void)
         }
         SEQ_ONE_KEY(KC_1)
         {
-            layer_invert(HHKB);
+            layer_invert(BASE);
+        }
+        SEQ_ONE_KEY(KC_2)
+        {
+            layer_invert(MAC);
         }
         SEQ_TWO_KEYS(KC_D, KC_D)
         {
