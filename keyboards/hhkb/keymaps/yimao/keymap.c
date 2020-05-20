f50502568581cedf54236a35d2f9d0ead15b11ee
Yimao Wang <yimao.wang@miotech.com>
2020-05-20 11:38:27 +0800

    update keymap, using RGUI

--- keyboards/hhkb/keymaps/yimao/keymap.c
+++ keyboards/hhkb/keymaps/yimao/keymap.c
@@ -4,7 +4,7 @@
 #include QMK_KEYBOARD_H
 
 #define BASE 0
-#define MAC 1
+#define PC 1
 #define HHKB 2
 #define MOUSE 3
 
@@ -27,22 +27,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |-----------------------------------------------------------|
      * |Shift(  |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift)|LEA|
      * `-----------------------------------------------------------'
-     *       |MO1|Alt  |         Space         |MO1  |Gui|
+     *       |MO1|Gui |         Space         |MO1  |Gui|
      *       `-------------------------------------------'
     */
     [BASE] = LAYOUT( //  default layer
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSLS, KC_GRV, 
         KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSPC, 
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, LT(MOUSE, KC_SCLN), KC_QUOT, KC_ENT, 
-        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LEAD, 
-        MO(HHKB), KC_LALT, KC_SPC, MO(HHKB), KC_RGUI),
+        KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_LEAD,
+        MO(HHKB), KC_LGUI, KC_SPC, KC_RGUI, KC_LALT),
 
-    [MAC] = LAYOUT( //  default layer
+    [PC] = LAYOUT( //  default layer
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, LT(MOUSE, KC_SCLN), KC_TRNS, KC_TRNS, 
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-        KC_TRNS, KC_LGUI, KC_TRNS, KC_TRNS, KC_LALT),
+        KC_TRNS, KC_LALT, KC_TRNS, KC_TRNS, KC_RGUI),
 
 
     /* HHKB Level (HHKB Fn)
@@ -129,7 +129,7 @@ void matrix_scan_user(void)
         leading = false;
         leader_end();
 
-        SEQ_ONE_KEY(KC_M)
+        SEQ_ONE_KEY(KC_G)
         {
             SEND_STRING("ownstars@gmail.com");
         }
@@ -143,7 +143,7 @@ void matrix_scan_user(void)
         }
         SEQ_ONE_KEY(KC_2)
         {
-            layer_invert(MAC);
+            layer_invert(PC);
         }
         SEQ_TWO_KEYS(KC_D, KC_D)
         {
